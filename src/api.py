7179aefbd98913c932df683a5322259291253f1d
Mikko Koho <mikko.koho@aalto.fi>
2018-01-10 15:01:35 +0200

    Use Celery

--- src/api.py
+++ src/api.py
@@ -6,15 +6,13 @@ Relevance feedback search API.
 import argparse
 import logging
 import pickle
-from hashlib import sha1
 
 import eventlet
-import numpy as np
-from flask import Flask, request, json
+from flask import Flask, request
 from flask_cors import CORS
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO
 
-from search import RFSearch_GoogleAPI
+from tasks import search_worker
 
 app = Flask(__name__)
 CORS(app)
@@ -26,49 +24,9 @@ search_cache = dict()
 
 eventlet.monkey_patch()
 
+
 # TODO: Cache scrape results separately
-
-
-def get_results(words):
-    query_hash = sha1(' '.join(words).encode("utf-8")).hexdigest()
-    cache_hit = search_cache.get(query_hash, {})
-    items = cache_hit.get('items')
-
-    if items:
-        log.info('Cache hit for search id %s' % query_hash)
-        return cache_hit
-
-    items = searcher.search(words)
-    # items = pickle.load(open('google_search_results.pkl', 'rb'))
-    log.debug('Got %s results through search' % len(items))
-
-    results = {'result_id': query_hash, 'items': items}
-    search_cache.update({query_hash: results})
-    return results
-
-
-def get_topics(results):
-    items = results.get('items')
-    query_hash = results.get('result_id')
-
-    cache_hit = search_cache.get(query_hash, {})
-    topic_words = cache_hit.get('topic_words')
-
-    if topic_words:
-        return results, topic_words
-
-    log.debug('Scraping for search id %s' % query_hash)
-    emit('search_status_msg', {'data': 'Scraping'})
-    items = searcher.scrape_contents(items)
-
-    log.debug('Topic modeling for search id %s' % query_hash)
-    emit('search_status_msg', {'data': 'Topic modeling'})
-    items, topic_words = searcher.topic_model(items)
-
-    results.update({'items': items, 'topic_words': topic_words})
-    search_cache.update({query_hash: results})
-
-    return results, topic_words
+# TODO: Cache everything to redis
 
 
 @app.route('/')
@@ -81,59 +39,14 @@ def search(query):
     log.info('Search API got query: %s' % query)
 
     if query:
-        emit('search_status_msg', {'data': 'Search with {}'.format(query['data'])})
-        search_words = query['data']['query'].split()
-        frontend_results = query['data'].get('results')
-
-        results = get_results(search_words)
-        items = results['items']
-
-        emit('search_words', {'data': search_words})
-
-        if not frontend_results:
-            emit('search_status_msg', {'data': 'Got {} results'.format(len(items))})
-            emit('search_ready', {'data': json.dumps(results)})
-
-        results, topic_words = get_topics(results)
-        items = results['items']
-
-        if frontend_results:
-            for item in items:
-                url = item['url']
-                thumb = next((res.get('thumb') for res in frontend_results if res.get('url') == url), None)
-                if 'topic' in item:
-                    thumb_words = topic_words[np.argmax(item['topic'])]
-                    if thumb is True:
-                        # TODO: Make magic happen
-                        new_words = thumb_words[:3]
-                        search_words += [word for word in new_words if word not in search_words]
-                    elif thumb is False:
-                        [search_words.remove(word) for word in thumb_words if word in search_words]
-
-            search_words = search_words[:20]
-
-        emit('search_words', {'data': search_words})
-
-        results = get_results(search_words)
-        items = results['items']
-
-        emit('search_status_msg', {'data': 'Got {} results'.format(len(items))})
-        emit('search_ready', {'data': json.dumps(results)})
-
-        results, topic_words = get_topics(results)
-        items = results['items']
-
-        emit('search_status_msg', {'data': 'Done'})
-        results.update({'items': items, 'topic_words': topic_words})
-
-        log.info('Dumping pickle')
-        pickle.dump(search_cache, open('search_cache.pkl', 'wb'))
+        search_worker.delay(query, request.sid)
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description=__doc__, fromfile_prefix_chars='@')
     argparser.add_argument("apikey", help="Google API key")
-    argparser.add_argument("--host", default=None, help="Host (e.g. 0.0.0.0)")
+    argparser.add_argument("--host", default='127.0.0.1', help="Host (e.g. 127.0.0.1)")
+    argparser.add_argument("--port", default=5000, help="Port (e.g. 5000)")
     argparser.add_argument("--loglevel", default='INFO', help="Logging level, default is INFO.",
                            choices=["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
 
@@ -142,7 +55,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=getattr(logging, args.loglevel),
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    searcher = RFSearch_GoogleAPI(args.apikey)
+    # searcher = RFSearch_GoogleAPI(args.apikey)
 
     if not search_cache:
         try:
@@ -150,4 +63,4 @@ if __name__ == "__main__":
         except FileNotFoundError:
             log.info('Search cache log file not found.')
 
-    socketio.run(app, host=args.host)
+    socketio.run(app, host=args.host, port=args.port)
---
+++ src/tasks.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+Relevance feedback search Celery tasks.
+"""
+import logging
+import os
+import pickle
+from hashlib import sha1
+
+import eventlet
+import numpy as np
+from celery import Celery
+from flask import json, Flask
+from flask_cors import CORS
+from flask_socketio import SocketIO
+
+from search import RFSearch_GoogleAPI
+
+eventlet.monkey_patch()
+
+log = logging.getLogger(__name__)
+
+celery_app = Celery('tasks', broker='redis://')
+socketio = SocketIO(message_queue='redis://')
+
+search_cache = dict()
+
+
+def get_results(searcher, words):
+    query_hash = sha1(' '.join(words).encode("utf-8")).hexdigest()
+    cache_hit = search_cache.get(query_hash, {})
+    items = cache_hit.get('items')
+
+    if items:
+        log.info('Cache hit for search id %s' % query_hash)
+        return cache_hit
+
+    items = searcher.search(words)
+    # items = pickle.load(open('google_search_results.pkl', 'rb'))
+    log.debug('Got %s results through search' % len(items))
+
+    results = {'result_id': query_hash, 'items': items}
+    search_cache.update({query_hash: results})
+    return results
+
+
+def get_topics(searcher, results, sessionid):
+    items = results.get('items')
+    query_hash = results.get('result_id')
+
+    cache_hit = search_cache.get(query_hash, {})
+    topic_words = cache_hit.get('topic_words')
+
+    if topic_words:
+        return results, topic_words
+
+    log.debug('Scraping for search id %s' % query_hash)
+    socketio.emit('search_status_msg', {'data': 'Scraping'}, room=sessionid)
+    items = searcher.scrape_contents(items)
+
+    log.debug('Topic modeling for search id %s' % query_hash)
+    socketio.emit('search_status_msg', {'data': 'Topic modeling'}, room=sessionid)
+    items, topic_words = searcher.topic_model(items)
+
+    results.update({'items': items, 'topic_words': topic_words})
+    search_cache.update({query_hash: results})
+
+    return results, topic_words
+
+
+@celery_app.task
+def search_worker(query, sessionid):
+    socketio.emit('search_status_msg', {'data': 'Search with {}'.format(query['data'])}, room=sessionid)
+    search_words = query['data']['query'].split()
+    frontend_results = query['data'].get('results')
+
+    apikey = os.environ["APIKEY"]
+    log.info(apikey)
+    searcher = RFSearch_GoogleAPI(apikey)
+
+    results = get_results(searcher, search_words)
+    items = results['items']
+
+    socketio.emit('search_words', {'data': search_words}, room=sessionid)
+
+    if not frontend_results:
+        socketio.emit('search_status_msg', {'data': 'Got {} results'.format(len(items))}, room=sessionid)
+        socketio.emit('search_ready', {'data': json.dumps(results)}, room=sessionid)
+
+    results, topic_words = get_topics(searcher, results, sessionid)
+    items = results['items']
+
+    if frontend_results:
+        for item in items:
+            url = item['url']
+            thumb = next((res.get('thumb') for res in frontend_results if res.get('url') == url), None)
+            if 'topic' in item:
+                thumb_words = topic_words[np.argmax(item['topic'])]
+                if thumb is True:
+                    # TODO: Make magic happen
+                    new_words = thumb_words[:3]
+                    search_words += [word for word in new_words if word not in search_words]
+                elif thumb is False:
+                    [search_words.remove(word) for word in thumb_words if word in search_words]
+
+        search_words = search_words[:20]
+
+    socketio.emit('search_words', {'data': search_words}, room=sessionid)
+
+    results = get_results(searcher, search_words)
+    items = results['items']
+
+    socketio.emit('search_status_msg', {'data': 'Got {} results'.format(len(items))}, room=sessionid)
+    socketio.emit('search_ready', {'data': json.dumps(results)}, room=sessionid)
+
+    results, topic_words = get_topics(searcher, results, sessionid)
+    items = results['items']
+
+    socketio.emit('search_status_msg', {'data': 'Done'}, room=sessionid)
+    results.update({'items': items, 'topic_words': topic_words})
+
+    log.info('Dumping pickle')
+    pickle.dump(search_cache, open('search_cache.pkl', 'wb'))
+
